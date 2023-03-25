610fb537181635f1cf3413eebd7d5ac2cab8b13a
hindicoder3 <112818265+hindicoder3@users.noreply.github.com>
2023-03-25 17:59:06 +0100

    Remove not longer needed index.d.ts

--- index.d.ts
+++
@@ -1,7 +0,0 @@
-declare module 'bingimageclient' {
-  class BingImageClient {
-    constructor(options: any)
-    getImages(prompt: string): Array<string>
-    downloadImages(urls: (RequestInfo | URL)[]): void
-  }
-}
